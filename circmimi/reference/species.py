f63f81a1594df26ab3214b27dfafd30b9a2124c4
chiangtw <chiangtw112358@gmail.com>
2020-01-07 11:19:05 +0800

    add NCBI tax_id

--- circmimi/reference/species.py
+++ circmimi/reference/species.py
@@ -1,23 +1,23 @@
 from collections import namedtuple
 
-Species = namedtuple("Species", ['key', 'name', 'fullname'])
+Species = namedtuple("Species", ['key', 'name', 'fullname', 'tax_id'])
 
 species_list = {
-    'ath': Species('ath', 'arabidopsis_thaliana', 'Arabidopsis thaliana'),
-    'bmo': Species('bmo', 'bombyx_mori', 'Bombyx mori'),
-    'bta': Species('bta', 'bos_taurus', 'Bos taurus'),
-    'cel': Species('cel', 'caenorhabditis_elegans', 'Caenorhabditis elegans'),
-    'cfa': Species('cfa', 'canis_familiaris', 'Canis familiaris'),
-    'dre': Species('dre', 'danio_rerio', 'Danio rerio'),
-    'dme': Species('dme', 'drosophila_melanogaster', 'Drosophila melanogaster'),
-    'gga': Species('gga', 'gallus_gallus', 'Gallus gallus'),
-    'hsa': Species('hsa', 'homo_sapiens', 'Homo sapiens'),
-    'mmu': Species('mmu', 'mus_musculus', 'Mus musculus'),
-    'osa': Species('osa', 'oryza_sativa', 'Oryza sativa'),
-    'ola': Species('ola', 'oryzias_latipes', 'Oryzias latipes'),
-    'oar': Species('oar', 'ovis_aries', 'Ovis aries'),
-    'rno': Species('rno', 'rattus_norvegicus', 'Rattus norvegicus'),
-    'ssc': Species('ssc', 'sus_scrofa', 'Sus scrofa'),
-    'tgu': Species('tgu', 'taeniopygia_guttata', 'Taeniopygia guttata'),
-    'xtr': Species('xtr', 'xenopus_tropicalis', 'Xenopus tropicalis')
+    'ath': Species('ath', 'arabidopsis_thaliana', 'Arabidopsis thaliana', '3702'),
+    'bmo': Species('bmo', 'bombyx_mori', 'Bombyx mori', '7091'),
+    'bta': Species('bta', 'bos_taurus', 'Bos taurus', '9913'),
+    'cel': Species('cel', 'caenorhabditis_elegans', 'Caenorhabditis elegans', '6239'),
+    'cfa': Species('cfa', 'canis_familiaris', 'Canis familiaris', '9615'),
+    'dre': Species('dre', 'danio_rerio', 'Danio rerio', '7955'),
+    'dme': Species('dme', 'drosophila_melanogaster', 'Drosophila melanogaster', '7227'),
+    'gga': Species('gga', 'gallus_gallus', 'Gallus gallus', '9031'),
+    'hsa': Species('hsa', 'homo_sapiens', 'Homo sapiens', '9606'),
+    'mmu': Species('mmu', 'mus_musculus', 'Mus musculus', '10090'),
+    'osa': Species('osa', 'oryza_sativa', 'Oryza sativa', '4530'),
+    'ola': Species('ola', 'oryzias_latipes', 'Oryzias latipes', '8090'),
+    'oar': Species('oar', 'ovis_aries', 'Ovis aries', '9940'),
+    'rno': Species('rno', 'rattus_norvegicus', 'Rattus norvegicus', '10116'),
+    'ssc': Species('ssc', 'sus_scrofa', 'Sus scrofa', '9823'),
+    'tgu': Species('tgu', 'taeniopygia_guttata', 'Taeniopygia guttata', '59729'),
+    'xtr': Species('xtr', 'xenopus_tropicalis', 'Xenopus tropicalis', '8364')
 }
