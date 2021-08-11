0e928da1bff8217cb65d565b555dd07982814c7f
chiangtw <chiangtw112358@gmail.com>
2021-08-11 16:53:47 +0800

    add prefix in the normal way

--- circmimi/utils.py
+++ circmimi/utils.py
@@ -1,11 +1,14 @@
 import os.path
 
 
-def add_prefix(filename, prefix):
-    prefix_dir, prefix_base = os.path.split(prefix)
+def add_prefix(filename, prefix, auto_dot=False):
+    if auto_dot:
+        prefix_dir, prefix_base = os.path.split(prefix)
 
-    filename = "{}.{}".format(prefix_base, filename)
-    filename = filename.lstrip('.')
-    filename = os.path.join(prefix_dir, filename)
+        filename = "{}.{}".format(prefix_base, filename)
+        filename = filename.lstrip('.')
+        filename = os.path.join(prefix_dir, filename)
+    else:
+        filename = prefix + filename
 
     return filename
